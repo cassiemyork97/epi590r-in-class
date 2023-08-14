44f1845f76a59d8ae7a653e4b1053997c3931675
cassiemyork97 <cassiemyork@gmail.com>
2023-08-14 11:35:06 -0400

    fix mistakes

--- R/clean-data-bad.R
+++ R/clean-data-bad.R
@@ -1,15 +1,17 @@
+library(tidyverse)
+library(dplyr)
+
+here::here()
+
 nlsy_cols <- c("glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
 							 "id", "nsibs", "samp", "race_eth", "sex", "region",
 							 "income", "res_1980", "res_2002", "age_bir")
 
-library(tidyverse)
-setwd("~/Documents/Teaching/Emory/epi590r-in-class/data/raw/")
-nlsy <- read_csv("nlsy.csv",
+nlsy <- read_csv("data/raw/nlsy.csv",
 								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
 								 skip = 1, col_names = nlsy_cols)
 
-library(dplyr)
-nlsy <- nlsy |>
+nlsy <- nlsy %>%
 	mutate(region_cat = factor(region, labels = c("Northeast", "North Central", "South", "West")),
 				 sex_cat = factor(sex, labels = c("Male", "Female")),
 				 race_eth_cat = factor(race_eth, labels = c("Hispanic", "Black", "Non-Black, Non-Hispanic")),
